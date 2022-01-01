a6526ba44446f2cf360d37d055b8d824589670a7
itbj <itbj00@163.com>
2022-01-01 22:14:07 +0800

    demoStyle

--- public/tiger/jsx.js
+++ public/tiger/jsx.js
@@ -11,10 +11,17 @@ let user={
     age: 2022
 };
 
+let sitename="Click here goto Cisco.com";
+let linkUrl="http://www.cisco.com";   
+let linkHome="/a.html"
+
 let myDOM=(
     <div>
         <div>{text} {num+1}</div>  {/* <br> must be paired.  */}
-        <div>{fun(user)}</div>
+        <div>{fun(user)}</div><br></br>
+        <div><a href={linkUrl}>{sitename}</a></div>
+        <div><a href={linkHome}>Back</a></div>
+        <p className="demoStyle">demoStyle</p>  {/* demoStyle in a. html style file.  */}
     </div>
 
 );
@@ -22,4 +29,8 @@ let myDOM=(
 ReactDOM.render(myDOM,document.getElementById("demoReact"));
 {/*
 https://youtu.be/tdmyawesFJE?list=PLwDQt7s1o9J7u0lzzlFQYx5sJ7GvZkA4i&t=850
+
+http://localhost:3000/tiger/www.cisco.com if linkUrl ="www.cisco.com", no http
+
+https://youtu.be/tdmyawesFJE?list=PLwDQt7s1o9J7u0lzzlFQYx5sJ7GvZkA4i&t=1536 .demoStyle{}
 */}
